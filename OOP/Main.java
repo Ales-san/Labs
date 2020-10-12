c127fde16e2adac9cabaefd3a93c19f7ac9e609b
Ales-san <45343131+Ales-san@users.noreply.github.com>
2020-10-12 12:17:01 +0300

    Create Main.java

---
+++ OOP/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Parser parser = new Parser();
+       try {
+            Data data = parser.parse("input.txt");
+            System.out.println(data.tryGetDouble("Common", "StatisterTimeMs"));
+            //System.out.println(data.tryGetInt("ADC_DEV", "BufferLenSeconds"));
+            System.out.println(data.tryGetString("ADC_DEV", "Driver"));
+            System.out.println(data.tryGetInt("LEGACY_XML", "ListenTcpPort"));
+            System.out.println(data.tryGetDouble("LEGACY_XML", "ListenTcpPort"));
+            System.out.println(data.tryGetString("DEBUG", "PlentySockMaxQSize"));
+            //System.out.println(data.tryGetString("SMTH", "IDK"));
+            //System.out.println(data.tryGetInt("LEGACY_XML", "Port"));
+            System.out.println(data.tryGetString("NEW0", "Smth"));
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
